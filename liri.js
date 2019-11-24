b0ae8af2b7c4ec1e730a5bcc3b9925da658a40b6
Caitlan Jeffrey <caitlanjeffrey@gmail.com>
2019-11-24 07:03:37 -0800

    saving key changes

--- liri.js
+++ liri.js
@@ -41,62 +41,76 @@ const concert = function() {
 
 // run songs function for switch/case
 const songs = function() {
+    if(argvTwo) {
+        spotify(argvTwo)
+    } else{
+        spotify("The Sign Ace of Base")
+    }
+}
 
-    // Spotify keys required
-    const spotify = new Spotify(keys.spotify);
+    function spotify(artist){
+        // Spotify keys required
+        const spotify = new Spotify(keys.spotify);
 
-    // applying argvTwo to spotify api call
-    spotify.search({ type: 'track', query: argvTwo }, function(err, data) {
+        // applying argvTwo to spotify api call
+        spotify.search({ type: 'track', query: artist }, function(err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        // loop for initial object information
-        for (let i = 0; i < 3; i++) {
-            console.log("Song: " + data.tracks.items[i].name);
-            console.log("Album: " + data.tracks.items[i].album.name);
-            console.log("URL: " + data.tracks.items[i].preview_url);
+            // loop for initial object information
+            for (let i = 0; i < 3; i++) {
+                console.log("Song: " + data.tracks.items[i].name);
+                console.log("Album: " + data.tracks.items[i].album.name);
+                console.log("URL: " + data.tracks.items[i].preview_url);
 
-            // empty array to push artist names 
-            var artistNames = []
+                // empty array to push artist names 
+                var artistNames = []
 
-            // artists is an object within the items object, created for loop to grab artist name values 
-            for (let j = 0; j < data.tracks.items[i].artists.length; j++){
-                artistNames.push(data.tracks.items[i].artists[j].name)
-            };
+                // artists is an object within the items object, created for loop to grab artist name values 
+                for (let j = 0; j < data.tracks.items[i].artists.length; j++){
+                    artistNames.push(data.tracks.items[i].artists[j].name)
+                };
             
-            console.log("Artist(s): " + artistNames.join(", "))
+                console.log("Artist(s): " + artistNames.join(", "))
 
-            console.log("***************************************".blue)
-        }
-    })
-}
+                console.log("***************************************".blue)
+            }
+        })
+    }
 
 // run movies function for switch/case
 const movies = function() {
-
-    // applying argvTwo to omdb api call
-    axios.get("http://www.omdbapi.com/?t=" + argvTwo + "&" + keys.omdbURL.key).then(function (response) {
-        // handle success
-        console.log("Title: " + response.data.Title)
-        console.log("Plot: " + response.data.Plot)
-        console.log("Actors: " + response.data.Actors)
-        console.log("Year: " + response.data.Year)
-        console.log("Country: " + response.data.Country)
-        console.log("Language: " + response.data.Language)
-        console.log("IMDB Rating: " + response.data.Ratings[0].Value)
-        console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
-
-        console.log("***************************************".yellow)
-
-    }).catch(function (error) {
-        // handle error
-        console.log(error);
-    }).finally(function () {
-        
-    });
+    if(argvTwo) {
+        movieSearch(argvTwo)
+    } else {
+        movieSearch("Mr. Nobody")
+    }
 }
 
+    function movieSearch(movie) {
+
+        // applying argvTwo to omdb api call
+        axios.get("http://www.omdbapi.com/?t=" + movie + "&" + keys.omdbURL.key).then(function (response) {
+            // handle success
+            console.log("Title: " + response.data.Title)
+            console.log("Plot: " + response.data.Plot)
+            console.log("Actors: " + response.data.Actors)
+            console.log("Year: " + response.data.Year)
+            console.log("Country: " + response.data.Country)
+            console.log("Language: " + response.data.Language)
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value)
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value)
+
+            console.log("***************************************".yellow)
+
+        }).catch(function (error) {
+            // handle error
+            console.log(error);
+        }).finally(function () {
+            
+        });
+    }
 // run doWhat function for switch/case
 const doWhat = function() {
     fs.readFile("random.txt", "utf8", function(err, data) {
@@ -106,6 +120,8 @@ const doWhat = function() {
     
     //Parse the JSON string to an object
         const doThisThing = JSON.parse(data);
+            //split data value
+            // parse to switch case function where [0] = argv1 [1]=arv2
     
     //Create two new arrays to contain the cats and dogs objects
         const dogs = [];
